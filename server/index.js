b3b2d57f256b7f816936b81d13dac87ceb31843d
h-zahar <hz.rifat@gmail.com>
2023-12-06 23:17:07 +0600

    Set Up WS Connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const { WebSocketServer } = require("ws");
+const url = require("url");
+const { v4: uuidv4 } = require("uuid");
 
 const port = 5000;
 
@@ -7,6 +9,14 @@ const server = http.createServer();
 
 const wsServer = new WebSocketServer({ server });
 
+wsServer.on("connection", (connection, request) => {
+  const { username } = url.parse(request.url, true).query;
+
+  const uuid = uuidv4();
+  console.log(username);
+  console.log(uuid);
+});
+
 server.listen(port, () => {
-  console.log("Server Running Happily at ", port);
+  console.log("Server Running Happily at", port);
 });
