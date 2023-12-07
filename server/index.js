73d724b26b580dc23cdc34634f7aa12d19c549f2
h-zahar <hz.rifat@gmail.com>
2023-12-07 21:24:09 +0600

    Implemented Broadcast System

--- server/index.js
+++ server/index.js
@@ -9,12 +9,53 @@ const server = http.createServer();
 
 const wsServer = new WebSocketServer({ server });
 
+const connections = {};
+const users = {};
+
+const broadcast = () => {
+  Object.keys(connections).forEach((uuid) => {
+    const connection = connections[uuid];
+    const message = JSON.stringify(users);
+    connection.send(message);
+  });
+};
+
+const handleMessage = (message, uuid) => {
+  const parsedMessage = JSON.parse(message.toString());
+
+  const user = users[uuid];
+  user["state"] = parsedMessage;
+
+  broadcast();
+
+  console.log(
+    `${user.username} updated their state: ${JSON.stringify(user.state)}`
+  );
+
+  //   console.log(parsedMessage);
+};
+
+const handleClose = (uuid) => {
+  delete connections[uuid];
+  delete users[uuid];
+
+  broadcast();
+};
+
 wsServer.on("connection", (connection, request) => {
   const { username } = url.parse(request.url, true).query;
 
   const uuid = uuidv4();
-  console.log(username);
-  console.log(uuid);
+
+  connections[uuid] = connection;
+
+  users[uuid] = {
+    username,
+    state: {},
+  };
+
+  connection.on("message", (message) => handleMessage(message, uuid));
+  connection.on("close", () => handleClose(uuid));
 });
 
 server.listen(port, () => {
