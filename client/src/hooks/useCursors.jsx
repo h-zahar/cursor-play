e50ce33ee59276ecf915efa903be19d265e464a1
h-zahar <hz.rifat@gmail.com>
2023-12-08 21:20:56 +0600

    Created Cursor Hook

---
+++ client/src/hooks/useCursors.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { PerfectCursor } from "perfect-cursors";
+
+export function useCursor(cb, point) {
+  const [pc] = React.useState(() => new PerfectCursor(cb));
+
+  React.useLayoutEffect(() => {
+    if (point) pc.addPoint(point);
+    return () => pc.dispose();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pc]);
+
+  const onPointChange = React.useCallback((point) => pc.addPoint(point), [pc]);
+
+  return onPointChange;
+}
