7e6f465185a5ba471c0390896aa24d90ec996a36
h-zahar <hz.rifat@gmail.com>
2023-12-08 21:21:41 +0600

    Implemented Cursor Feature on Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,18 +1,32 @@
 import { useEffect, useRef } from "react";
 import useWebSocket from "react-use-websocket";
 import throttle from "lodash.throttle";
+import { Cursor } from "../Cursor";
 
 const Home = ({ username }) => {
   const WS_URL = "ws://localhost:5000";
 
-  const { sendJsonMessage } = useWebSocket(WS_URL, {
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
     queryParams: { username },
   });
 
   const THROTTLE = 50;
   const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE));
 
+  const renderCursors = (users) => {
+    return Object.keys(users).map((uuid) => {
+      const user = users[uuid];
+
+      return <Cursor key={uuid} point={[user.state.x, user.state.y]} />;
+    });
+  };
+
   useEffect(() => {
+    sendJsonMessage({
+      x: 0,
+      y: 0,
+    });
+
     window.addEventListener("mousemove", (e) => {
       sendJsonMessageThrottled.current({
         x: e.clientX,
@@ -20,6 +34,10 @@ const Home = ({ username }) => {
       });
     });
   }, []);
+
+  if (lastJsonMessage) {
+    return <>{renderCursors(lastJsonMessage)}</>;
+  }
 
   return <div>{username}</div>;
 };
